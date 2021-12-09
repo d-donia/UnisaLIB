cac08a6507cc95063ed81683c7ea580364e8443e
Sabato Celentano <s.celentano16@studenti.unisa.it>
2021-12-09 22:27:15 +0100

    Mockup 12 aggiornato

--- app/src/main/java/com/example/unisalib/MainActivity.java
+++ app/src/main/java/com/example/unisalib/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.mockup15);
+        setContentView(R.layout.mockup12);
     }
 }
