d262b925f3d790633415576f13017741cbf8b9d1
d-donia <d.donia@studenti.unisa.it>
2022-01-03 17:37:46 +0100

    Modifica Prestito, PrestitoPresenter e DettagliLibroUtenteUnisaActivity per gestione creazione prestito

--- app/src/main/java/presenter/prestitopresenter/PrestitoPresenter.java
+++ app/src/main/java/presenter/prestitopresenter/PrestitoPresenter.java
@@ -7,40 +7,54 @@ import com.loopj.android.http.JsonHttpResponseHandler;
 import com.loopj.android.http.RequestParams;
 
 import org.json.JSONArray;
+import org.json.JSONException;
 import org.json.JSONObject;
+
+import java.lang.reflect.ParameterizedType;
 
 import cz.msebera.android.httpclient.Header;
 import model.prestitomanagement.Prestito;
 import view.interfacciautenteunisa.DettagliLibroUtenteUnisaActivity;
 
 public class PrestitoPresenter {
-    static final String GenericURL="http://192.168.1.61:8080/UnisaLIBServer/PrestitoPresenter";
+    static final String GenericURL="http://192.168.1.7:8080/UnisaLIBServer/PrestitoPresenter";
     private AsyncHttpClient client=new AsyncHttpClient();
     public void creaPrestito(Prestito p) {
         String MYURL=GenericURL + "/crea-prestito";
         RequestParams params;
         params=new RequestParams();
-        params.put("Prestito", p);
+        params.put("prestito", Prestito.toJson(p));
         client.post(MYURL, params, new JsonHttpResponseHandler(){
             @Override
             public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
                 super.onSuccess(statusCode, headers, response);
-                Toast.makeText(DettagliLibroUtenteUnisaActivity.getAppContext(), "Prestito avvenuto con successo. Ritirare il libro in biblioteca", Toast.LENGTH_LONG);
+                try {
+                    Prestito prestito = Prestito.fromJson(response);
+                    Toast.makeText(DettagliLibroUtenteUnisaActivity.getAppContext(), "Prestito avvenuto con successo. Ritirare il libro in biblioteca", Toast.LENGTH_LONG).show();
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
             }
 
             @Override
             public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONObject errorResponse) {
                 super.onFailure(statusCode, headers, throwable, errorResponse);
+                System.out.println("Prestito fallito");
+                Toast.makeText(DettagliLibroUtenteUnisaActivity.getAppContext(), errorResponse.toString() + ". Riprovare più tardi", Toast.LENGTH_LONG).show();
             }
 
             @Override
             public void onFailure(int statusCode, Header[] headers, Throwable throwable, JSONArray errorResponse) {
                 super.onFailure(statusCode, headers, throwable, errorResponse);
+                System.out.println("Prestito fallito");
+                Toast.makeText(DettagliLibroUtenteUnisaActivity.getAppContext(), errorResponse.toString() + ". Riprovare più tardi", Toast.LENGTH_LONG).show();
             }
 
             @Override
             public void onFailure(int statusCode, Header[] headers, String responseString, Throwable throwable) {
                 super.onFailure(statusCode, headers, responseString, throwable);
+                System.out.println("Prestito fallito");
+                Toast.makeText(DettagliLibroUtenteUnisaActivity.getAppContext(), responseString + ". Riprovare più tardi", Toast.LENGTH_LONG).show();
             }
         });
     }
