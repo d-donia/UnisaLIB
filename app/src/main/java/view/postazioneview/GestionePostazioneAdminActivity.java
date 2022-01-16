c8632ddedf44242f512af150f8b80e38f2b0158d
Paky29 <p.somma11@studenti.unisa.it>
2022-01-16 18:30:01 +0100

    aggiunta pagina gestione postazioni

--- app/src/main/java/view/postazioneview/GestionePostazioneAdminActivity.java
+++ app/src/main/java/view/postazioneview/GestionePostazioneAdminActivity.java
@@ -1,10 +1,9 @@
-package view.libroview;
+package view.postazioneview;
 
 import android.app.Activity;
 import android.app.AlertDialog;
 import android.content.Context;
 import android.content.DialogInterface;
-import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
@@ -19,30 +18,28 @@ import com.google.gson.JsonSyntaxException;
 
 import model.utentemanagement.Utente;
 import presenter.FacadePresenter;
-import view.utenteview.HomeAdminActivity;
 
-public class GestioneLibroAdminActivity extends Activity {
+public class GestionePostazioneAdminActivity extends Activity {
+    private FacadePresenter fp;
+    private Utente u;
     private static Context context;
-    FacadePresenter fp;
-    Utente u;
-
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.admin_gestione_libri);
-        context=getApplicationContext();
+        setContentView(R.layout.admin_gestione_postazioni);
         fp=new FacadePresenter();
+        context=getApplicationContext();
         ImageButton userIB=findViewById(R.id.gestioneLibroIB);
         userIB.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                AlertDialog confermaLogout = new AlertDialog.Builder(GestioneLibroAdminActivity.this).
+                AlertDialog confermaLogout = new AlertDialog.Builder(GestionePostazioneAdminActivity.this).
                         setTitle("Logout").
                         setMessage("Sicuro di voler uscire?").
                         setPositiveButton(R.string.si, new DialogInterface.OnClickListener() {
                             @Override
                             public void onClick(DialogInterface dialog, int which) {
-                                fp.logout(GestioneLibroAdminActivity.getAppContext());
+                                fp.logout(GestionePostazioneAdminActivity.getAppContext());
                                 finish();
                             }
                         }).
@@ -54,6 +51,9 @@ public class GestioneLibroAdminActivity extends Activity {
                         }).show();
             }
         });
+        Button aggiungiButton=findViewById(R.id.aggiungiPostButton);
+        Button modificaButton=findViewById(R.id.modificaPostButton);
+
         SharedPreferences userSession = PreferenceManager.getDefaultSharedPreferences(this);
         try {
             u = Utente.fromJson(userSession.getString("Utente", ""));
@@ -61,25 +61,14 @@ public class GestioneLibroAdminActivity extends Activity {
         catch (JsonSyntaxException e){
             e.printStackTrace();
         }
-        Button addLibro= findViewById(R.id.addButton);
-        Button modifyLibro=findViewById(R.id.modifyButton);
 
-        addLibro.setOnClickListener(new View.OnClickListener() {
+        modificaButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                fp.informazioniAggiuntaLibro();
-            }
-        });
-
-        modifyLibro.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                fp.mostraRicercaLibri(u.isAdmin(),getApplicationContext());
+                fp.mostraRicercaPostazioni(u.isAdmin(), getApplicationContext());
             }
         });
     }
 
-    public static Context getAppContext(){
-        return context;
-    }
+    public static Context getAppContext(){return context;}
 }
--- app/src/main/java/view/utenteview/HomeAdminActivity.java
+++ app/src/main/java/view/utenteview/HomeAdminActivity.java
@@ -1,12 +1,15 @@
 package view.utenteview;
 
 import android.app.Activity;
+import android.app.AlertDialog;
 import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 import androidx.preference.PreferenceManager;
 
@@ -17,19 +20,44 @@ import com.google.gson.JsonSyntaxException;
 import model.utentemanagement.Utente;
 import presenter.FacadePresenter;
 import view.libroview.GestioneLibroAdminActivity;
+import view.postazioneview.GestionePostazioneAdminActivity;
 
 public class HomeAdminActivity extends Activity {
-    Utente u;
-    FacadePresenter fp;
+    private Utente u;
+    private FacadePresenter fp;
+    private static Context context;
 
     public static Context getAppContext() {
-        return getAppContext();
+        return context;
     }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.admin_home);
+        context=getApplicationContext();
+        ImageButton userIB=findViewById(R.id.adminIB);
+        userIB.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog confermaLogout = new AlertDialog.Builder(HomeAdminActivity.this).
+                        setTitle("Logout").
+                        setMessage("Sicuro di voler uscire?").
+                        setPositiveButton(R.string.si, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                fp.logout(HomeAdminActivity.getAppContext());
+                                finish();
+                            }
+                        }).
+                        setNegativeButton(R.string.no, new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                dialog.cancel();
+                            }
+                        }).show();
+            }
+        });
 
         fp=new FacadePresenter();
         SharedPreferences userSession = PreferenceManager.getDefaultSharedPreferences(this);
@@ -46,8 +74,9 @@ public class HomeAdminActivity extends Activity {
         mgmtPrenotazioneButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                System.out.println("Sono qui");
-                fp.mostraRicercaPostazioni(u.isAdmin(), getApplicationContext());
+                Intent i=new Intent();
+                i.setClass(getApplicationContext(), GestionePostazioneAdminActivity.class);
+                startActivity(i);
             }
         });
 
